178f35314025bda0ee0cb8bb2ffb115b2390a026
rbrahul2020 <rahulbhatt.phy18@itbhu.ac.in>
2022-05-21 23:31:02 +0530

    added ids to images array & given showing default image functionality from JS

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 import {images} from './data.js'
 
-console.log(images);
+const length = images.length;
+
+// add id to each obj of "images" array
+for(let i=0;i<length;i++)
+{
+   images[i].id = i;
+}
 
 const imagesListContainer = document.querySelector(".images-list-container");
-console.log(imagesListContainer);
 
-const length = images.length;
+
 
 // iterate all the images
 const content = images.reduce(function(content, image){
-    content += `<div class="image-info" id="2">
+    content += `<div class="image-info" id=${image.id}>
     <img class="left-image" src=${image.previewImage} />
     <h6 class="left-image-title">${image.title}</h6>
     </div>`;
@@ -20,3 +25,23 @@ const content = images.reduce(function(content, image){
 // assign images-list-container data
 imagesListContainer.innerHTML = content;
 
+const imagesInfo = document.querySelector(".images-info");
+
+// show given "image" in right side
+function showImage(image)
+{
+    const id = image.id;
+    const previewImage = image.previewImage;
+    const title = image.title;
+
+    const imageInfo = document.querySelector(`.image-info[id="${id}"]`);
+    imageInfo.classList.add("image-info-selected");
+    const rightImage = document.querySelector(".right-image");
+    rightImage.src = previewImage;
+    const rightImageTitle = document.querySelector(".right-image-title");
+    rightImageTitle.value = title;
+    rightImageTitle.id = id;
+}
+
+const defaultShowImageID = 0;
+showImage(images[defaultShowImageID]);
